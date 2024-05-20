f1139fefadf85606e89ea7cf60ec6b598384167f
이성종 <dltjdwhd1235@naver.com>
2024-05-20 23:38:22 +0900

    Revert "2024-05-20"
    This reverts commit fa7755d24f722d093e9d6c1d818ea56b8fbf7018.

--- src/main/java/com/bamboo/repository/fileAllowedRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bamboo.repository;
-
-
-import com.bamboo.entity.FileConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface fileAllowedRepository extends JpaRepository<FileConfig,Long> {
-}
---
+++ src/main/java/com/codehows/sbd/constant/Role.java
@@ -0,0 +1,5 @@
+package com.codehows.sbd.constant;
+
+public enum Role {
+    USER, ADMIN
+}
