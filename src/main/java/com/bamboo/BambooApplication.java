9db2a7b86e38a0a4a5408cb399707e5449798a23
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-05-28 12:06:02 +0900

    bamboo 최종본

---
+++ src/main/java/com/bamboo/BambooApplication.java
@@ -0,0 +1,28 @@
+package com.bamboo;
+
+import com.bamboo.service.MemberService;
+import com.bamboo.service.fileAllowedService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import org.springframework.boot.CommandLineRunner;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BambooApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(BambooApplication.class, args);}
+
+    @Bean
+    public CommandLineRunner run(fileAllowedService fileAllowedService, MemberService memberService) {
+        return args -> {
+            fileAllowedService.defaultFileAllowed();
+            memberService.saveAdmin();
+        };
+    }
+}
